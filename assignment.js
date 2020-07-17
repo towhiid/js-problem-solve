c5e5820445627f7750bfc6518631c35d9d6a8f63
towhiid <towhid3008@gmail.com>
2020-07-17 22:21:12 +0600

    part 3 done

--- assignment.js
+++ assignment.js
@@ -7,11 +7,41 @@ function feetToMile(feet) {
         return mile;
     }
 }
-var result = feetToMile(-3);
+var result = feetToMile(1);
 console.log(result);
 
 // woodCalculator
 
+function woodCalculator(chair, table, bed){
+    var chairCount = chair*1;
+    var tableCount = table*3;
+    var bedCount = bed*5;
+    var totalWood = chairCount + tableCount + bedCount;
+
+    if(chair<0 || table<0 || bed<0){
+        return "please enter a positive value"
+    }else{
+        return totalWood;
+    }
+}
+    var woodResult = woodCalculator(2,3,2);
+    console.log(woodResult);
+
 // brickCalculator
 
+
+
 // tinyFriend
+
+function tinyFriend(names){
+    var tiny = names[0];
+    for(var i=0; i<names.length; i++){
+        var currentName = names[i];
+        if(currentName.length<tiny.length){
+            tiny = currentName;
+        }
+    }
+    return tiny;
+}
+var myFriend = tinyFriend(["Ashik", "Opu", "Topu", "Ri", "mashuk"]);
+console.log(myFriend);
