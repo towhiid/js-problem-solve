8b8faee47c43c84f1d6961a818732bafff272afb
towhiid <towhid3008@gmail.com>
2020-07-17 11:07:03 +0600

    first commit

---
+++ assignment.js
@@ -0,0 +1,13 @@
+// feetToMile
+
+// woodCalculator
+
+// brickCalculator
+
+// tinyFriend
+function (feetToMile) {
+    var feet = 420;
+    var mile = feet*0.00062;
+    return = feetToMile(mile);
+
+}
